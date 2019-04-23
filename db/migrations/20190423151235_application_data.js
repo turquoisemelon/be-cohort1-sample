9b80963a5f1e3704efd74831d466600191feff9d
Purvi Kanal <purvikanal92@gmail.com>
2019-04-23 15:24:38 -0400

    add migrations for application data

---
+++ db/migrations/20190423151235_application_data.js
@@ -0,0 +1,38 @@
+exports.up = knex =>
+  knex.schema
+    .createTable("cohorts", table => {
+      table.increments();
+      table.string("name").notNullable();
+      table.enu("cohort_type", ["frontend", "backend", "design"]).notNullable();
+      table.datetime("start_date").notNullable();
+      table.datetime("end_date").notNullable();
+      table.text("welcome_text").notNullable();
+      table.text("thank_you_text").notNullable();
+    })
+
+    .createTable("applications", table => {
+      table.increments();
+      table
+        .integer("cohort_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("cohorts");
+      table
+        .integer("applicant_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("applicants");
+      table
+        .boolean("accepted_test")
+        .notNullable()
+        .defaultsTo(false);
+      table
+        .boolean("accepted_cohort")
+        .notNullable()
+        .defaultsTo(false);
+    });
+
+exports.down = knex =>
+  knex.schema.dropTable("applications").dropTable("cohorts");
