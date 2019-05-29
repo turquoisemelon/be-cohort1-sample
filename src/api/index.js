4a8d0713f9620b29c34c58646b7e2495741890a2
Tessa Thornton <tessathornton@gmail.com>
2019-05-29 10:07:14 -0400

    comment out all but health router

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 
-const { mockAuthenticate, authenticateRequest } = require("./authentication");
-const { authorizeRequest } = require("./authorization");
+// const { mockAuthenticate, authenticateRequest } = require("./authentication");
+// const { authorizeRequest } = require("./authorization");
 const { healthRouter } = require("../routes/health/health.router");
-const { usersRouter } = require("../routes/users/users.router");
-const { cohortsRouter } = require("../routes/cohorts/cohorts.router");
-const {
-  applicationsRouter
-} = require("../routes/applications/applications.router");
+// const { usersRouter } = require("../routes/users/users.router");
+// const { cohortsRouter } = require("../routes/cohorts/cohorts.router");
+// const {
+//   applicationsRouter
+// } = require("../routes/applications/applications.router");
 
 const router = express.Router();
 
 // unauthenticated endpoints
 router.use("/health", healthRouter);
 
-router.use(authenticateRequest);
-router.use(authorizeRequest);
+// router.use(authenticateRequest);
+// router.use(authorizeRequest);
 
-//authenticated endpoints
-router.use("/users", usersRouter);
-router.use("/cohorts", cohortsRouter);
-router.use("/applications", applicationsRouter);
+// //authenticated endpoints
+// router.use("/users", usersRouter);
+// router.use("/cohorts", cohortsRouter);
+// router.use("/applications", applicationsRouter);
 
 module.exports = router;
