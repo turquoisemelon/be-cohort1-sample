be7165a7b3239b92224d7f670e70ee5a75aae26e
Tessa Thornton <tessathornton@gmail.com>
2019-05-29 12:34:40 -0400

    knexfile syntax error

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: "pg",
   connection: {
-    host: process.env.NODE_ENV === "development" ? "db" || process.env.RDS_HOSTNAME,
-    user: process.env.NODE_ENV === "development" ? "admin" || process.env.RDS_USERNAME,
-    password: process.env.NODE_ENV === "development" ? "admin" || process.env.RDS_PASSWORD,
-    database: process.env.NODE_ENV === "development" ? "bridge-applications-local" || process.env.RDS_DB
+    host: process.env.NODE_ENV === "development" ? "db" : process.env.RDS_HOSTNAME,
+    user: process.env.NODE_ENV === "development" ? "admin" : process.env.RDS_USERNAME,
+    password: process.env.NODE_ENV === "development" ? "admin" : process.env.RDS_PASSWORD,
+    database: process.env.NODE_ENV === "development" ? "bridge-applications-local" : process.env.RDS_DB
   },
   migrations: {
     directory: `${__dirname}/db/migrations`
