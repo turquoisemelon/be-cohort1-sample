f63c5d2690e9e0dfc54547c19e14214ee986d0d5
Tessa Thornton <tessathornton@gmail.com>
2019-05-29 12:33:04 -0400

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     host: process.env.NODE_ENV === "development" ? "db" || process.env.RDS_HOSTNAME,
     user: process.env.NODE_ENV === "development" ? "admin" || process.env.RDS_USERNAME,
     password: process.env.NODE_ENV === "development" ? "admin" || process.env.RDS_PASSWORD,
-    database: process.env.NODE_ENV === "development" ? "bridge-applications-local" || process.env.RDS_PASSWORD
+    database: process.env.NODE_ENV === "development" ? "bridge-applications-local" || process.env.RDS_DB
   },
   migrations: {
     directory: `${__dirname}/db/migrations`
